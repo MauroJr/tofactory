61e2688d588c83d981593ea2af819fde9c0c52e7
Mauro Junior <mauropereirajr@gmail.com>
2016-03-07 23:59:34 -0300

    now toFactory return a function instead a object

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ module.exports = toFactory;
 const toString = Object.prototype.toString;
 
 function toFactory(fn) {
-    let obj = Object.create(null);
     
-    obj.create = function (spec) {
+    Create.create = function create(spec) {
+        return Create(spec);
+    };
+    
+    function Create(spec) {
         let _return;
         spec = spec || {};
         _return = fn(spec);
@@ -14,7 +17,8 @@ function toFactory(fn) {
         }
         _return = Object.assign(Object.create(null), _return);
         return Object.freeze(_return);
-    };
+    }
 
-    return Object.freeze(obj);
+    return Object.freeze(Create);
+    
 }
